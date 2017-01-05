f1d1ea43d86efdc2b105e16dce0316923517915d
Logan Riggs <lriggs@tableau.com>
2017-01-05 09:29:13 -0800

    Increase version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='tdvt',
-    version='1.1.9',
+    version='1.1.10',
     author='Tableau',
     author_email='github@tableau.com',
     url='https://github.com/tableau/TODO',
