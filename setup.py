d79c42b97d66179e15a66ab72cafe974dc8b1bcc
Logan Riggs <lriggs@tableau.com>
2017-03-09 12:39:19 -0800

    Update version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='tdvt',
-    version='1.1.19',
+    version='1.1.20',
     author='Tableau',
     author_email='github@tableau.com',
     url='https://github.com/tableau/TODO',
