5366f91b091328bee1f6381ddb0ef1fe09889dfb
Logan Riggs <lriggs@tableau.com>
2017-07-27 13:16:10 -0700

    Version number.

--- tdvt/tdvt.py
+++ tdvt/tdvt.py
@@ -8,7 +8,7 @@ import sys
 if sys.version_info[0] < 3:
     raise EnvironmentError("TDVT requires Python 3 or greater.")
 
-__version__ = '1.1.35'
+__version__ = '1.1.36'
 
 import os
 import argparse
