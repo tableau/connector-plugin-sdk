1c1be22671bd0a9e0acbcb01bafbc8135ca28dcf
Anindita Talukdar <atalukdar@tableau.com>
2017-08-03 12:38:53 -0700

    update version number

--- tdvt/tdvt.py
+++ tdvt/tdvt.py
@@ -8,7 +8,7 @@ import sys
 if sys.version_info[0] < 3:
     raise EnvironmentError("TDVT requires Python 3 or greater.")
 
-__version__ = '1.1.38'
+__version__ = '1.1.39'
 
 import os
 import argparse
