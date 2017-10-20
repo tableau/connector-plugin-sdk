6f7c9e537689de1797c7e30a43a0ea7cbf779750
Michael Arvold <marvold@tableau.com>
2017-10-20 13:45:21 -0700

    Minor: Update version number

--- tdvt/tdvt.py
+++ tdvt/tdvt.py
@@ -8,7 +8,7 @@ import sys
 if sys.version_info[0] < 3:
     raise EnvironmentError("TDVT requires Python 3 or greater.")
 
-__version__ = '1.1.46'
+__version__ = '1.1.47'
 
 import os
 import argparse
