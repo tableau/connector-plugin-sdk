2200a93f6b309e7d0473c2af24ef523960182bc7
Logan Riggs <lriggs@tableau.com>
2018-06-18 09:45:51 -0700

    Update version

--- tdvt/tdvt.py
+++ tdvt/tdvt.py
@@ -8,7 +8,7 @@ import sys
 if sys.version_info[0] < 3:
     raise EnvironmentError("TDVT requires Python 3 or greater.")
 
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 
 from zipfile import ZipFile
 import argparse
