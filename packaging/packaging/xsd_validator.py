e34b7b9b0ae85a2b593b971032042bff640b91af
Patrick VanderKnyff <pvanderknyff@tableau.com>
2019-04-01 11:45:54 -0700

    Add file size check, can now be run independantly

--- packaging/packaging/xsd_validator.py
+++ packaging/packaging/xsd_validator.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from connector_file import ConnectorFile
 
+MAX_FILE_SIZE = 1024 * 256 # This is based on the max file size we will load on the Tableau side
 PATH_TO_XSD_FILES = Path("validation").absolute()
 VALID_XML_EXTENSIONS = ['tcd', 'tdr', 'tdd', 'xml'] # These are the file extensions that we will validate
 
@@ -11,7 +12,7 @@ VALID_XML_EXTENSIONS = ['tcd', 'tdr', 'tdd', 'xml'] # These are the file extensi
 XSD_DICT = {
     "manifest": "connector_plugin_manifest_latest",
     "connection-dialog": "tcd_latest",
-    "connection_resolver": "tdr_latest",
+    "connection-resolver": "tdr_latest",
     "dialect": "tdd_latest",
     "resource": "connector_plugin_resources_latest"
 }
@@ -19,6 +20,7 @@ XSD_DICT = {
 # files_list: List of files to validate
 # folder_path: path to folder that contains the files.
 # Return value: True if all xml files pass validation, false if they do not or there is an error
+# Assumption: The file_list and folder_path are correct, and do not point to files that don't exist or directories
 def validate_xsd(files_list, folder_path):
     
     print("Starting XSD validation...")
@@ -31,7 +33,7 @@ def validate_xsd(files_list, folder_path):
         print("Error: validate_xsd: input list is empty")
         return False
 
-    all_files_valid = True
+    xml_violations_found = 0
     xml_violations_buffer = "XML violations found.\n\n" # If xml violations are found, we save them here and print at end of method
 
     for file_to_test in files_list:
@@ -47,29 +49,37 @@ def validate_xsd(files_list, folder_path):
         xsd_file = GetXSDFile(file_to_test)
         
         if not xsd_file:
-            print("Error: No valid XSD for file type:" + file_to_test.file_type)
-            all_files_valid = False
+            xml_violations_buffer+= "Error: No valid XSD for file type:" + file_to_test.file_type
+            xml_violations_found+=1
             continue
 
         manifest_schema = xmlschema.XMLSchema(str(PATH_TO_XSD_FILES / Path(xsd_file)))
         saved_error = None
+
+        # If the file is too big, we shouldn't try and parse it, just log the violation and move on
+        if path_to_file.stat().st_size > MAX_FILE_SIZE:
+            xml_violations_found+=1
+            xml_violations_buffer += file_to_test.file_name + " exceeds maximum size of " + str(int(MAX_FILE_SIZE / 1024)) + " KB"
+            continue
         
         # Try to validate the xml. If the xml validation error is thrown, save the violation information to the buffer
         try:
             manifest_schema.validate(str(path_to_file))
         except xmlschema.validators.exceptions.XMLSchemaValidationError:
             saved_error = sys.exc_info()[1]
-            all_files_valid = False
-            xml_violations_buffer += "File: " + str(path_to_file) + "\n" + str(saved_error)
+            xml_violations_found+=1
+            xml_violations_buffer += "File: " + file_to_test.file_name + "\n" + str(saved_error)
             print("Validation failed.")
 
-    if all_files_valid:
+    if xml_violations_found <= 0:
         print("No XSD violations found")
     else:
         print(xml_violations_buffer)
-        print("XSD validation failed")
+        print("XSD validation failed!\n" + str(xml_violations_found) + " violations found.")
 
-    return all_files_valid
+    print(str(len(files_list)) + " xml files parsed.")
+
+    return xml_violations_found <= 0
 
 # Return the XSD file to test against
 def GetXSDFile(file_to_test):
@@ -80,15 +90,25 @@ def GetXSDFile(file_to_test):
         return None
 
 
-#For testing, will remove and add unit tests once initial version is done
+# Pass in a folder to test. Calls the file list generator and validates the xml files against the XSDs.
 if __name__ == "__main__":
-    hardcoded_plugin_folder = Path("C:/Users/pvanderknyff/Documents/connector-plugins/postgres_odbc")
-    #hardcoded_plugin_folder = Path("samples/plugins/postgres_odbc")
-    #files_list = [ConnectorFile("manifest.xml", "manifest")]
+    
+    if len(sys.argv) < 2:
+        print("Too few arguments. You must pass in the path to a folder containing the xml files.")
+        sys.exit()
+
+    folder_to_test = Path(sys.argv[1])
+    
+    if not folder_to_test.is_dir():
+        print("Error: " + str(folder_to_test) + " does not exist or is not a directory")
+        sys.exit()
+
+    # TODO: Once the plugin files list generator is completed, remove the ugly hardcoded list
     files_list = [
         ConnectorFile("manifest.xml", "manifest"),
         ConnectorFile("connection-dialog.tcd", "connection-dialog"),
         ConnectorFile("connectionBuilder.js", "script"),
         ConnectorFile("dialect.tdd", "dialect"),
-        ConnectorFile("connection-resolver.tdr", "connection-resolver")]
-    validate_xsd(files_list, hardcoded_plugin_folder)
+        ConnectorFile("connectionResolver.tdr", "connection-resolver")]
+
+    validate_xsd(files_list, folder_to_test)
