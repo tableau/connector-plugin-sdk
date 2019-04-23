673dea958e83f517ccfc683580894410aa62bbdd
Ross Brown <rosswbrown@users.noreply.github.com>
2019-04-23 11:29:09 -0700

    TestJarPackager clean up created packages

--- packaging/tests/test_jar_packager.py
+++ packaging/tests/test_jar_packager.py
@@ -21,9 +21,16 @@ class TestJarPackager(unittest.TestCase):
             ConnectorFile("resources-en_US.xml", "resource")]
         source_dir = TEST_FOLDER / Path("valid_connector")
         dest_dir = TEST_FOLDER / Path("packaged-connector/")
+        package_name = "test.taco"
 
-        create_jar(source_dir, files_list, "test.taco", dest_dir)
-        self.assertTrue(os.path.isfile(dest_dir/Path("test.taco")), "taco file doesn't exist")
+        create_jar(source_dir, files_list, package_name, dest_dir)
+
+        path_to_test_file = dest_dir / Path(package_name)
+        self.assertTrue(os.path.isfile(path_to_test_file), "taco file doesn't exist")
+
+        if path_to_test_file.exists():
+            path_to_test_file.unlink()
+
 
     def test_jdk_create_jar(self):
         files_list = [
@@ -35,8 +42,12 @@ class TestJarPackager(unittest.TestCase):
             ConnectorFile("resources-en_US.xml", "resource")]
         source_dir = TEST_FOLDER / Path("valid_connector")
         dest_dir = TEST_FOLDER / Path("packaged-connector-by-jdk/")
+        package_name = "test.taco"
 
-        jdk_create_jar(source_dir, files_list, "test.taco", dest_dir)
-        self.assertTrue(os.path.isfile(dest_dir/Path("test.taco")), "taco file doesn't exist")
+        jdk_create_jar(source_dir, files_list, package_name, dest_dir)
 
+        path_to_test_file = dest_dir / Path(package_name)
+        self.assertTrue(os.path.isfile(path_to_test_file), "taco file doesn't exist")
 
+        if path_to_test_file.exists():
+            path_to_test_file.unlink()
