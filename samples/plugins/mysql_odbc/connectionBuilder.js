963530c5da7b9e503869307a487457d01e65f80f
Luke Petschauer <luke@lukewrites.com>
2019-07-16 13:16:08 -0700

    REFACTOR after review

--- samples/plugins/mysql_odbc/connectionBuilder.js
+++ samples/plugins/mysql_odbc/connectionBuilder.js
@@ -2,10 +2,27 @@
 {
     var params = {};
 
-    params["SERVER"] = attr["server"];
+    params["SERVER"] = attr[connectionHelper.attributeServer];
     params["PORT"] = attr["port"];
-    params["UID"] = attr["username"];
-    params["PWD"] = attr["password"];
+    params["UID"] = attr[connectionHelper.attributeUsername];
+    params["PWD"] = attr[connectionHelper.attributePassword];
+    params["DATABASE"] = attr[connectionHelper.attributeDatabase];
+    params["INITSTMT"] = attr[connectionHelper.attributeInitialSQL];
+
+    if (attr["sslmode"] == "require" && attr["sslfile"] != undefined && attr["sslfile"] != "")
+    {
+        params["SSLCA"] = attr["sslfile"];
+        params["SSLMODE"] = "required";
+    }
+
+    params["OPTION"] = "1048576"
+
+    if (parseFloat(driverLocator.LocateDriverVersion(attr)) >= 8.0)
+    {
+        params["default_auth"] = "mysql_native_password";
+    }
+
+
     var formattedParams = [];
 
     formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
@@ -13,6 +30,11 @@
     for (var key in params)
     {
         formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
+    }
+
+    if (attr["odbc-connect-string-extra"] != "")
+    {
+        formattedParams.push(attr["odbc-connect-string-extras"])
     }
 
     return formattedParams;
