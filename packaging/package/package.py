da930c81fd1b65c9b28d20af71251e299cade278
Luke Petschauer <luke@lukewrites.com>
2019-08-01 15:45:22 -0700

    REFACTOR spacing for pep8

--- packaging/package/package.py
+++ packaging/package/package.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from pathlib import Path
 
 from .helper import init_logging
@@ -12,15 +13,21 @@ PACKAGED_EXTENSION = ".taco"
 
 
 def create_arg_parser():
-    parser = argparse.ArgumentParser(description="Tableau Connector Packaging Tool: package and sign connector files into a single Tableau Connector (" + PACKAGED_EXTENSION + ") file.")
+    parser = argparse.ArgumentParser(description="Tableau Connector Packaging Tool: package and sign connector files into a single Tableau Connector (" + PACKAGED_EXTENSION + ") file.")  # noqa: E501
     parser.add_argument('input_dir', help='path to directory of connector files to package and sign')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose output', required=False)
-    parser.add_argument('-l', '--log', dest='log_path', help='path of logging output', default='packaging_log.txt')
-    parser.add_argument('--validate-only', dest='validate_only', action='store_true', help='runs package validation steps only', required=False)
-    parser.add_argument('-d', '--dest', dest='dest', help='destination folder for packaged connector', default='packaged-connector')
-    parser.add_argument('--package-only', dest='package_only', action='store_true',  help='package a taco only, skip signing', required=False)
-    parser.add_argument('-a', '--alias', dest='alias', help='alias identifying the private key to be used to sign taco file', required=False)
-    parser.add_argument('-ks', '--keystore', dest='keystore', help='keystore location, default is the jks file in user home directory', required=False)
+    parser.add_argument('-l', '--log', dest='log_path', help='path to directory for logging output',
+                        default=os.getcwd())
+    parser.add_argument('--validate-only', dest='validate_only', action='store_true',
+                        help='runs package validation steps only', required=False)
+    parser.add_argument('-d', '--dest', dest='dest', help='destination folder for packaged connector',
+                        default='packaged-connector')
+    parser.add_argument('--package-only', dest='package_only', action='store_true',
+                        help='package a taco only, skip signing', required=False)
+    parser.add_argument('-a', '--alias', dest='alias',
+                        help='alias identifying the private key to be used to sign taco file', required=False)
+    parser.add_argument('-ks', '--keystore', dest='keystore',
+                        help='keystore location, default is the jks file in user home directory', required=False)
     return parser
 
 
