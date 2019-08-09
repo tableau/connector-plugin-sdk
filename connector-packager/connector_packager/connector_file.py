b693d91c875323efae52d32b09f4866e06cd5d2e
Luke Petschauer <lpetschauer@tableau.com>
2019-08-09 14:35:19 -0700

    REFACTOR: use forward reference in connector_file.py

--- connector-packager/connector_packager/connector_file.py
+++ connector-packager/connector_packager/connector_file.py
@@ -12,8 +12,8 @@ class ConnectorFile:
         # Remove the leading period.
         return ext if not ext else ext[1:]
 
-    def __lt__(self, other: ConnectorFile) -> bool:
+    def __lt__(self, other: 'ConnectorFile') -> bool:
         return self.file_name < other.file_name
 
-    def __eq__(self, other: ConnectorFile) -> bool:
+    def __eq__(self, other: 'ConnectorFile') -> bool:
         return self.file_name == other.file_name and self.file_type == other.file_type
