251066b776b710d5fe80680ac8ef139ed03474cd
Jainam Shah <jainamcse@gmail.com>
2019-09-01 10:11:59 -0700

    Update connectionBuilder.js
    Add code snippet to use odbc-connect-string-extras set as customization in manifest.xml

--- samples/components/resolvers/connectionBuilder.js
+++ samples/components/resolvers/connectionBuilder.js
@@ -14,6 +14,22 @@
 
     //You can write to the Tableau log file (tabprotosrv.txt). This requires Debug level logging to see (-DLogLevel=Debug).
     logging.log("Hi")
+    
+    //If odbc-connect-string-extras is set in the manifest.xml file then the key value pairs can be extracted and 
+    //inserted in the params Map. 
+    //For Example : If we have the line : <customization name="odbc-connect-string-extras" value="key1=value1"/> in manifest.xml
+    //then the below code snippet will insert the key1 , value1 pair in the params map , params["key1"] = "value1"
+    //Finally the line ';key1=value1' will be appended to the ConnectString.
+    var odbcConnectStringExtrasMap = {};
+    if ("odbc-connect-string-extras" in attr) 
+    {
+        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr["odbc-connect-string-extras"]);
+    }
+    for (var key in odbcConnectStringExtrasMap)
+    {
+        params[key] = odbcConnectStringExtrasMap[key];
+    }
+    
     var formattedParams = [];
     //Format the attributes as 'key=value'. By default some values are escaped or wrapped in curly braces to follow the ODBC standard, but you can also do it here if needed.
     for (var key in params)
@@ -25,7 +41,7 @@
     formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
 
     //The result will look like (search for ConnectString in tabprotosrv.log):
-    //ConnectString: Host=teradata.mydomain.com;UID=test;PWD=********;DATETIMEFORMAT=ABC;DRIVER={Teradata Database ODBC Driver 16.20}"}}
+    //ConnectString: Host=teradata.mydomain.com;UID=test;PWD=********;DATETIMEFORMAT=ABC;DRIVER={Teradata Database ODBC Driver 16.20}"}};
     return formattedParams;
 })
 
