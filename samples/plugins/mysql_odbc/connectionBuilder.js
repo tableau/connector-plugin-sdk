c0680f196771b425907d81e382fec260266f33fa
Jainam Shah <jainamcse@gmail.com>
2019-09-03 14:39:26 -0700

    Update connectionBuilder.js

--- samples/plugins/mysql_odbc/connectionBuilder.js
+++ samples/plugins/mysql_odbc/connectionBuilder.js
@@ -14,10 +14,10 @@
     var formattedParams = [];
     
     var odbcConnectStringExtrasMap = {};
-    const attributeODBCConnectionStringExtras = "odbc-connect-string-extras";
-    if (attributeODBCConnectionStringExtras in attr) 
+    const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
+    if (attributeODBCConnectStringExtras in attr) 
     {
-        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectionStringExtras]);
+        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
     }
     for (var key in odbcConnectStringExtrasMap)
     {
