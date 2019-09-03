7e23c16791c6f5c90ab8fd4db1e8318a5fdc02fc
Jainam Shah <jainamcse@gmail.com>
2019-09-03 12:08:54 -0700

    Update connectionBuilder.js
    Add code snippet to use odbc-connect-string-extras set in mysql_odbc

--- samples/plugins/mysql_odbc/connectionBuilder.js
+++ samples/plugins/mysql_odbc/connectionBuilder.js
@@ -12,17 +12,22 @@
     params["OPTION"] = "1048576"
 
     var formattedParams = [];
+    
+    var odbcConnectStringExtrasMap = {};
+    if ("odbc-connect-string-extras" in attr) 
+    {
+        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr["odbc-connect-string-extras"]);
+    }
+    for (var key in odbcConnectStringExtrasMap)
+    {
+        params[key] = odbcConnectStringExtrasMap[key];
+    }
 
     formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
 
     for (var key in params)
     {
         formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
-    }
-
-    if (attr["odbc-connect-string-extra"] != "")
-    {
-        formattedParams.push(attr["odbc-connect-string-extras"])
     }
 
     return formattedParams;
