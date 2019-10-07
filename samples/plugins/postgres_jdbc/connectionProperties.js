761016daa75bbcc500fd3f3d29645e81e182fc87
Saugat Tripathi <stripathi@tableau.com>
2019-10-07 16:35:54 -0700

    Update postgres_jdbc with optional fields

--- samples/plugins/postgres_jdbc/connectionProperties.js
+++ samples/plugins/postgres_jdbc/connectionProperties.js
@@ -2,11 +2,16 @@
     var props = [];
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
+    props["logLevel"] = attr[connectionHelper.attributeVendor1];
+    props["protocolVersion"] = attr[connectionHelper.attributeVendor2];
+    props["charSet"] = attr[connectionHelper.attributeVendor3];
+
 
     if (attr[connectionHelper.attributeSSLMode] == "require")
     {        
         props["ssl"] = "true";
         props["sslmode"] = "require";
+        
     }
 
     var formattedProps = [];
