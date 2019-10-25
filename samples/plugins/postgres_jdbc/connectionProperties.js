a38fa1848b3e8a78706ebb7d86b1a9f5dc2cf3f0
Luke Petschauer <lukewrites@users.noreply.github.com>
2019-10-25 15:10:48 -0700

    Revert "Vendor attributes"

--- samples/plugins/postgres_jdbc/connectionProperties.js
+++ samples/plugins/postgres_jdbc/connectionProperties.js
@@ -2,9 +2,6 @@
     var props = [];
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
-    props["logLevel"] = attr[connectionHelper.attributeVendor1];
-    props["protocolVersion"] = attr[connectionHelper.attributeVendor2];
-    props["charSet"] = attr[connectionHelper.attributeVendor3];
 
     if (attr[connectionHelper.attributeSSLMode] == "require")
     {        
