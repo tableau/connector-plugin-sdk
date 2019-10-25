f5ff013a2616abefb2df10a88cc9c6393e2b59a7
Luke Petschauer <lukewrites@users.noreply.github.com>
2019-10-25 15:10:56 -0700

    Revert "Added service to the required attribute list"

--- samples/components/dialogs/new_text_field/connectionBuilder.js
+++ samples/components/dialogs/new_text_field/connectionBuilder.js
@@ -2,22 +2,20 @@
 {
     var params = {};
 
-    params["SERVER"] = attr2[connectionHelper.attributeServer];
-    params["PORT"] = attr2[connectionHelper.attributePort];
-    params["DATABASE"] = attr2[connectionHelper.attributeDatabase];
-    params["UID"] = attr2[connectionHelper.attributeUsername];
-    params["PWD"] = attr2[connectionHelper.attributePassword];
+    params["SERVER"] = attr2["server"];
+    params["PORT"] = attr2["port"];
+    params["DATABASE"] = attr2["dbname"];
+    params["UID"] = attr2["username"];
+    params["PWD"] = attr2["password"];
     params["BOOLSASCHAR"] = "0";
     params["LFCONVERSION"] = "0";
     params["UseDeclareFetch"] = "1";
     params["Fetch"] = "2526";
 
     //Pull out the attributes from the connection dialog.
-    //You can preserve these attributes by adding service and vendor attributes to the required attribute list.
-    params["SERVICE_TEST"] = attr2[connectionHelper.attributeService];
-    params["VENDOR1_TEST"] = attr2[connectionHelper.attributeVendor1];
-    params["VENDOR2_TEST"] = attr2[connectionHelper.attributeVendor2];
-    params["VENDOR3_TEST"] = attr2[connectionHelper.attributeVendor3];
+    //You can preserve these attributes by adding service and attributeWarehouse to the required attribute list.
+    params["WH_TEST"] = attr2[connectionHelper.attributeWarehouse];
+    params["SERVICE_TEST"] = attr2["service"];
 
     var formattedParams = [];
 
