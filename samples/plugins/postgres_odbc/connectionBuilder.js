5c1a082dd303c4b790955335ff3732b5af8aec3f
Luke Petschauer <lpetschauer@tableau.com>
2019-11-04 15:10:09 -0800

    Revert "Merge pull request #266 from tableau/jainam1995-patch-4"
    This reverts commit 59296eba65a4ebe20cbe4273d8fd00221246706e, reversing
    changes made to 1c168ca54500582ce0dafa2685fc8121c5ad78df.

--- samples/plugins/postgres_odbc/connectionBuilder.js
+++ samples/plugins/postgres_odbc/connectionBuilder.js
@@ -12,22 +12,6 @@
     params["UseDeclareFetch"] = "1";
     params["Fetch"] = "2048";
 
-    if (attr[connectionHelper.attributeSSLMode] == "require")
-    {
-        params["sslmode"] = "require";
-    }
-
-    var odbcConnectStringExtrasMap = {};
-    const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
-    if (attributeODBCConnectStringExtras in attr)
-    {
-        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
-    }
-    for (var key in odbcConnectStringExtrasMap)
-    {
-        params[key] = odbcConnectStringExtrasMap[key];
-    }
-
     var formattedParams = [];
 
     formattedParams.push(connectionHelper.formatKeyValuePair(driverLocator.keywordDriver, driverLocator.locateDriver(attr)));
