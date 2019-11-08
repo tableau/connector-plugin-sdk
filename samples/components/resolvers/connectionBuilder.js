c7554bbf1dcf98be9174f78b7de9a84bfa8672ba
Luke Petschauer <luke@lukewrites.com>
2019-11-07 16:29:46 -0800

    Bring Jainam's changes back into vNext dev.

--- samples/components/resolvers/connectionBuilder.js
+++ samples/components/resolvers/connectionBuilder.js
@@ -14,6 +14,24 @@
 
     //You can write to the Tableau log file (tabprotosrv.txt). This requires Debug level logging to see (-DLogLevel=Debug).
     logging.log("Hi")
+    
+    //If odbc-connect-string-extras is defined then the key value pairs can be extracted and 
+    //inserted in the params Map. 
+    //For Example : If we have "odbc-connect-string-extras" set to  "key1=value1"
+    //then the below code snippet will insert the key1 , value1 pair in the params map , params["key1"] = "value1"
+    //Finally the line ';key1=value1' will be appended to the ConnectString.
+    var odbcConnectStringExtrasMap = {};
+    const attributeODBCConnectStringExtras = "odbc-connect-string-extras";
+    
+    if (attributeODBCConnectStringExtras in attr) 
+    {
+        odbcConnectStringExtrasMap = connectionHelper.ParseODBCConnectString(attr[attributeODBCConnectStringExtras]);
+    }
+    for (var key in odbcConnectStringExtrasMap)
+    {
+        params[key] = odbcConnectStringExtrasMap[key];
+    }
+    
     var formattedParams = [];
     //Format the attributes as 'key=value'. By default some values are escaped or wrapped in curly braces to follow the ODBC standard, but you can also do it here if needed.
     for (var key in params)
