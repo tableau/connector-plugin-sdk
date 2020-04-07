0db7c0e7423fbfddf5b7638b1855f6cf8215c5d1
Patrick VanderKnyff <pvanderknyff@tableau.com>
2020-04-07 16:06:53 -0700

    Delete version test

--- connector-packager/tests/test_version.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from connector_packager import version
-
-
-class TestPackage(unittest.TestCase):
-
-    def test_version(self):
-        self.assertTrue(version.__version__ == '1.0.0')
