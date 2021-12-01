cb7e2ddd0c18d2f1b92c4d50469304fc1713b4c9
Luke Petschauer <lpetschauer@tableau.com>
2021-11-30 21:14:28 -0800

    flake8 nits

--- tdvt/tdvt/config_gen/datasource_list.py
+++ tdvt/tdvt/config_gen/datasource_list.py
@@ -332,17 +332,15 @@ def load_test(config, test_dir=get_root_dir()):
                 pass
         # Add smoke tests
         elif connection_test in section:
-            test_path = 'logicaltests/setup/connection_test/'
-
             try:
                 all_ini_sections.remove(section)
                 test_config.add_logical_test('StaplesConnectionTest', STAPLES_TDS, sect.get(KEY_EXCLUSIONS, ''),
-                                             test_config.get_logical_test_path('logicaltests/setup/connection_test/setup.staples.*.'), test_dir,
-                                             get_password_file(sect), get_expected_message(sect),  True,
+                                             test_config.get_logical_test_path('logicaltests/setup/connection_test/setup.staples.*.'),  # noqa: E501
+                                             test_dir, get_password_file(sect), get_expected_message(sect),  True,
                                              get_is_test_enabled(sect, 'StaplesTestEnabled'), False)
                 test_config.add_logical_test('CastCalcsConnectionTest', CALCS_TDS, sect.get(KEY_EXCLUSIONS, ''),
-                                             test_config.get_logical_test_path('logicaltests/setup/connection_test/setup.calcs.*.'), test_dir,
-                                             get_password_file(sect), get_expected_message(sect), True,
+                                             test_config.get_logical_test_path('logicaltests/setup/connection_test/setup.calcs.*.'),  # noqa: E501
+                                             test_dir, get_password_file(sect), get_expected_message(sect), True,
                                              get_is_test_enabled(sect, 'CastCalcsTestEnabled'), False)
             except KeyError as e:
                 logging.debug(e)
