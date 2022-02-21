fb009e1aecbcbed6be8f8d06ed88d92ca02031ec
Luke Petschauer <lpetschauer@tableau.com>
2022-02-20 21:12:25 -0800

    add constants.py

--- tdvt/tdvt/constants.py
+++ tdvt/tdvt/constants.py
@@ -1,7 +1,18 @@
-DEFAULT_TDVT_LOGGING = [
-    ''
-]
-
-PERFLAB_TDVT_LOGGING = [
-    ''
+PERFLAB_TDVT_LOGGING_HEADERS = [
+    "TestGroup",
+    "TestSubGroup",
+    "Test",
+    "TestComment1",
+    "TestComment2",
+    "TestComment3",
+    "Iteration",
+    "IterationStartTime",
+    "IterationEndTime",
+    "ErrorString",
+    "IterationComment1",
+    "IterationComment2",
+    "IterationComment3",
+    "MetricResourceType",
+    "MetricResourceInstance",
+    "Result"
 ]
