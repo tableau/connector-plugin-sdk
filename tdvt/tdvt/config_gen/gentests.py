570d739de69a4229f0edeef713df3c406b7cadf8
Luke Petschauer <lpetschauer@tableau.com>
2022-03-09 14:38:06 -0800

    Reformat gentests.py

--- tdvt/tdvt/config_gen/gentests.py
+++ tdvt/tdvt/config_gen/gentests.py
@@ -2,21 +2,19 @@
     Generate datasource specific logical query files based on a set of genericized input files.
 """
 
-import sys
-import os
-import shutil
-import argparse
-from .templates import *
-from string import Template
-import re
 import glob
-
-from ..constants import CALCS_FIELDS, STAPLES_FIELDS
-from ..resources import *
 import logging
+import re
+import shutil
+from string import Template
 from typing import Dict
 
+from .templates import *
+from ..constants import CALCS_FIELDS, STAPLES_FIELDS
+from ..resources import *
+
 debug = False
+
 
 def get_logical_config_templates(ds_registry):
     all_templates = template_attributes.copy()
@@ -29,8 +27,10 @@ def get_logical_config_templates(ds_registry):
 
     return all_templates
 
+
 def get_logical_config_template(ds_registry, config_name):
     return get_logical_config_templates(ds_registry)[config_name]
+
 
 def check_logical_config_boolean_attr_value(attrs: Dict, key: str) -> bool:
     if key not in attrs:
@@ -44,6 +44,7 @@ def check_logical_config_boolean_attr_value(attrs: Dict, key: str) -> bool:
     logging.warning("Only True or False are valid for attribute " + key)
     return True
 
+
 def get_customized_table_name(attributes, base_table):
     table_prefix = ""
     if 'tablePrefix' in attributes:
@@ -52,7 +53,7 @@ def get_customized_table_name(attributes, base_table):
     table_name = attributes['tablename']
 
     t = Template(table_name)
-    #These substitution holders are in templates.py.
+    # These substitution holders are in templates.py.
     if check_logical_config_boolean_attr_value(attributes, 'tablenameUpper'):
         table_name = t.substitute(dsName=base_table.upper())
     else:
@@ -63,7 +64,7 @@ def get_customized_table_name(attributes, base_table):
     elif 'calcsnameLower' in attributes and 'calcs' in table_name.lower():
         table_name = table_name.lower()
     elif 'staplesnameLower' in attributes and 'staples' in table_name.lower():
-        table_name= table_name.lower()
+        table_name = table_name.lower()
 
     if 'tablenamePrefix' in attributes:
         table_name = attributes['tablenamePrefix'] + table_name
@@ -71,6 +72,7 @@ def get_customized_table_name(attributes, base_table):
         table_name += attributes['tablenamePostfix']
 
     return table_prefix + '[' + table_name + ']'
+
 
 def get_new_field_name(field, attrs):
     new_field = field
@@ -101,16 +103,18 @@ def get_new_field_name(field, attrs):
 
     return new_field
 
+
 def get_field_name_map(fields, attrs):
     m = {}
     for f in fields:
         m[f] = get_new_field_name(f, attrs)
     return m
 
+
 def get_modified_line(line, attrs, fields, field_name_map):
     new_line = line
     if 'test name' in line:
-         return new_line
+        return new_line
     if 'query-function' in line:
         return new_line
     if 'runquery-column' in line:
@@ -141,27 +145,26 @@ def process_test_file( filename, output_dir, staples_fields, calcs_fields, ds_re
     if debug:
         print("Test " + test_name)
 
-
     fields = staples_fields + calcs_fields  # TODO change this
 
     ds_file_map = {}
     for ds in get_logical_config_templates(ds_registry):
-        setup_file = open( os.path.join( output_dir, 'setup.' + test_name + '.' + ds + '.xml'), 'w', encoding='utf-8' )
-        field_name_map = get_field_name_map(fields,  get_logical_config_template(ds_registry, ds))
+        setup_file = open(os.path.join(output_dir, 'setup.' + test_name + '.' + ds + '.xml'), 'w', encoding='utf-8')
+        field_name_map = get_field_name_map(fields, get_logical_config_template(ds_registry, ds))
         ds_file_map[ds] = (setup_file, field_name_map)
 
-    #Go through all the configurations in templates.py and generate logical configs for them.
+    # Go through all the configurations in templates.py and generate logical configs for them.
     for line in input_file:
         for ds in ds_file_map:
             new_line = get_modified_line(line, get_logical_config_template(ds_registry, ds), fields, ds_file_map[ds][1])
-            ds_file_map[ds][0].write( new_line )
+            ds_file_map[ds][0].write(new_line)
 
     for ds in ds_file_map:
         ds_file_map[ds][0].close()
 
     input_file.close()
 
-    #Go through all the ini files and see if any of them define a logical config. Generate test files for those.
+    # Go through all the ini files and see if any of them define a logical config. Generate test files for those.
 
 
 def process_text(ds, text, attributes, fields, field_map):
@@ -172,16 +175,20 @@ def process_text(ds, text, attributes, fields, field_map):
         new_text += new_line + '\n'
     return new_text
 
+
 def get_config_text(config_name, config_attributes, fields, config_field_map):
     configs = []
-    sample_text = [ 'Name = $Name$', 'Calcs = $Calcs$', 'Staples = $Staples$', 'Camel Case = ' + fields[0], 'bool0 = ' + fields[1], 'Date = ' + fields[2] ]
+    sample_text = ['Name = $Name$', 'Calcs = $Calcs$', 'Staples = $Staples$', 'Camel Case = ' + fields[0],
+                   'bool0 = ' + fields[1], 'Date = ' + fields[2]]
 
     cfg = process_text(config_name, sample_text, config_attributes, fields, config_field_map)
     configs.append(cfg)
     return configs
 
+
 def list_config(ds_registry, config_name):
     return list_configs(ds_registry, config_name)
+
 
 def list_configs(ds_registry, target_config_name=None):
     configs = []
@@ -192,9 +199,10 @@ def list_configs(ds_registry, target_config_name=None):
         if target_config_name and config_name != target_config_name:
             continue
         cfg_template = get_logical_config_template(ds_registry, config_name)
-        field_name_map = get_field_name_map(fields,  cfg_template)
+        field_name_map = get_field_name_map(fields, cfg_template)
         configs += get_config_text(config_name, cfg_template, fields, field_name_map)
     return configs
+
 
 def clean_create_dir(new_dir):
     try:
@@ -203,12 +211,14 @@ def clean_create_dir(new_dir):
     except OSError:
         return
 
+
 def create_dir(new_dir):
-    #Make the output dir if needed, otherwise continue on our way since its there.
+    # Make the output dir if needed, otherwise continue on our way since its there.
     try:
         os.makedirs(new_dir)
     except OSError:
         return
+
 
 def generate_logical_files(input_dir, output_dir, ds_registry, force=False):
     base_output_dir = output_dir
@@ -218,17 +228,17 @@ def generate_logical_files(input_dir, output_dir, ds_registry, force=False):
 
     staples_fields = STAPLES_FIELDS
 
-    #Go through input and top level subdirs. Create those in the output and then process the files.
+    # Go through input and top level subdirs. Create those in the output and then process the files.
     for root, dirs, files in os.walk(input_dir):
         for name in dirs:
             input_dir = os.path.join(root, name)
             output_dir = os.path.join(base_output_dir, name)
             any_test_files = glob.glob(os.path.join(output_dir, 'setup*xml'))
             if not any_test_files or force:
-                print ("Generating test files from: " + str(input_dir))
-                print ("Writing test files to: " + str(output_dir))
+                print("Generating test files from: " + str(input_dir))
+                print("Writing test files to: " + str(output_dir))
                 clean_create_dir(output_dir)
                 for input_root, input_dirs, input_files in os.walk(input_dir):
                     for input_filename in input_files:
-                        process_test_file( os.path.join(input_root, input_filename), output_dir, staples_fields, calcs_fields, ds_registry )
-
+                        process_test_file(os.path.join(input_root, input_filename), output_dir, staples_fields,
+                                          calcs_fields, ds_registry)
